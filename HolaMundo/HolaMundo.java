522a7efd77934d8a3223e649bca9f0924b4e9572
aleavd <avellanedamail@gmail.com>
2025-10-17 09:57:40 +0200

    Update HolaMundo.java

--- HolaMundo/HolaMundo.java
+++ HolaMundo/HolaMundo.java
@@ -2,7 +2,8 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo...");
 	}
+
 
   }
